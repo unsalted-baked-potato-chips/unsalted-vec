2f9c0113e303b24e5954f87900a4eed43e3d3133
Linux User <robert@DESKTOP-QS3F3ME.localdomain>
2022-01-02 08:11:57 +0000

    removed unnecesary nested structs

--- include/uvec.h
+++ include/uvec.h
@@ -6,27 +6,23 @@ struct _vec2_f {
     float j;
 };
 
-struct _vec3_f {
-    union {
-        struct {
-            float i;
-            float j;
-            float k;
-        };
-        struct _vec2_f vec2;
+union _vec3_f {
+    struct {
+        float i;
+        float j;
+        float k;
     };
+    struct _vec2_f vec2;
 };
 
-struct _vec4_f {
-    union {
-        struct {
-            float i;
-            float j;
-            float k;
-            float l;
-        };
-        struct _vec3_f vec3;
+union _vec4_f {
+    struct {
+        float i;
+        float j;
+        float k;
+        float l;
     };
+    union _vec3_f vec3;
 };
 
 struct _vec2_d {
@@ -34,35 +30,31 @@ struct _vec2_d {
     double j;
 };
 
-struct _vec3_d {
-    union {
-        struct {
-            double i;
-            double j;
-            double k;
-        };
-        struct _vec2_d vec2;
+union _vec3_d {
+    struct {
+        double i;
+        double j;
+        double k;
     };
+    struct _vec2_d vec2;
 };
 
-struct _vec4_d {
-    union {
-        struct {
-            double i;
-            double j;
-            double k;
-            double l;
-        };
-        struct _vec3_d vec3;
+union _vec4_d {
+    struct {
+        double i;
+        double j;
+        double k;
+        double l;
     };
+    union _vec3_d vec3;
 };
 
 typedef struct _vec2_f vec2_f;
-typedef struct _vec3_f vec3_f;
-typedef struct _vec4_f vec4_f;
+typedef union  _vec3_f vec3_f;
+typedef union  _vec4_f vec4_f;
 
 typedef struct _vec2_d vec2_d;
-typedef struct _vec3_d vec3_d;
-typedef struct _vec4_d vec4_d;
+typedef union  _vec3_d vec3_d;
+typedef union  _vec4_d vec4_d;
 
 #endif //UNSALTED_UVEC_H
